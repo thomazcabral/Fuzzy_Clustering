8b00a6e497de88bf77a1bb8380e5bd6d0cee2a56
Thomaz Cabral <127772709+thomazcabral@users.noreply.github.com>
2025-04-11 19:10:29 -0300

    Update fr.py
    tava certo desde o início

--- metrics/fr.py
+++ metrics/fr.py
@@ -1,21 +1,28 @@
 import numpy as np
-from scipy.spatial.distance import pdist, squareform
+
+def pertinence_distance(delta_k, delta_k_linha, c):
+    # calcula a distância entre δ_k e δ_k' (matrizes de pertinência)
+    return (1/c) * np.sum((delta_k - delta_k_linha) ** 2)
 
 def fuzzy_rand_index(particao1, particao2):
-    n = particao1.shape[0] # número de observações
-
-    EP = 1 - squareform(pdist(particao1, 'euclidean'))  # E_P(x,x') = 1 - ||P(x) - P(x')||
-    EQ = 1 - squareform(pdist(particao2, 'euclidean'))  # E_Q(x,x') = 1 - ||Q(x) - Q(x')||
-
-    # soma das discordâncias
-    total_discordance = np.sum(np.abs(EP - EQ)) / 2  # soma para todo (x,x') ∈ C |E_P(x,x') - E_Q(x,x')|
-
-    # normaliza pelo número de pares
+    n = particao1.shape[0]
+    c = particao1.shape[1]
+    total_sum = 0
+    for k in range(n):
+        for k_linha in range(k+1, n):
+            # calcula a métrica para P
+            delta_k = particao1[k]
+            delta_k_prime = particao1[k_linha]
+            EP = 1 - pertinence_distance(delta_k, delta_k_prime, c)
+            # calcula a métrica para Q
+            delta_k_Q = particao2[k]
+            delta_k_prime_Q = particao2[k_linha]
+            EQ = 1 - pertinence_distance(delta_k_Q, delta_k_prime_Q, c)
+            total_sum += np.abs(EP - EQ)
     denominador = n * (n - 1) / 2
-    d = total_discordance / denominador  # distância entre as partições
+    return 1 - (total_sum / denominador)
 
-    return 1 - d  # valor final
-
+# Exemplo
 particao1 = np.array([[0.5, 0.2, 0.3], [0.1, 0.8, 0.1], [1, 0, 0]])  # P
 particao2 = np.array([[0.9, 0.1, 0], [0.7, 0.1, 0.2], [0.3, 0.2, 0.4]])  # Q
 

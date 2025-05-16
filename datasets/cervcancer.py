c0844d0df3dc83baa098435e98318d0d60893aca
Thomaz Cabral <127772709+thomazcabral@users.noreply.github.com>
2025-05-16 15:01:12 -0300

    Create cervcancer.py

---
+++ datasets/cervcancer.py
@@ -0,0 +1,12 @@
+# The dataset contains 19 attributes regarding ca cervix behavior risk with class label is ca_cervix with 1 and 0 as values which means the respondent with and without ca cervix, respectively.
+
+# pip3 install ucimlrepo
+
+from ucimlrepo import fetch_ucirepo
+dataset = fetch_ucirepo(id=537)
+
+df = pd.DataFrame(dataset.data.features)
+
+if dataset.data.targets is not None:o
+    targets = pd.DataFrame(dataset.data.targets)
+    df = pd.concat([df, targets], axis=1)
